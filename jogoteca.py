6b3d6ce0d9b578f6091766497d915462dadbfbf7
Italo <italo.ads31@gmail.com>
2020-09-11 08:48:42 -0300

    Criando os metodos de insert

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 class Jogo:
@@ -8,13 +8,32 @@ class Jogo:
         self.console = console
 
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Super Mario Bros', 'Ação', 'Nitendo')
-    jogo2 = Jogo('The Wicher', 'Aventura', 'Pc')
-    jogo3 = Jogo('Super Mario', 'Ação', 'Wii')
+jogo1 = Jogo('Super Mario Bros', 'Ação', 'Nitendo')
+jogo2 = Jogo('The Wicher', 'Aventura', 'Pc')
+jogo3 = Jogo('Super Mario', 'Ação', 'Wii')
 
-    games = [jogo1, jogo2, jogo3]
+games = [jogo1, jogo2, jogo3]
+
+
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Games', jogos=games)
 
-app.run()
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo ='Novo Game')
+
+
+@app.route('/criar', methods =['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    jogo = Jogo(nome, categoria, console)
+    games.append(jogo)
+    return render_template('lista.html', titulo='Games', jogos=games)
+
+
+app.run(debug=True)
