1f480de9af418dbb7faa2126ce455ea8e6e0c06d
Italo <italo.ads31@gmail.com>
2020-09-11 07:58:41 -0300

    Tornando dinamico alguns atributos do templates/lista.html

--- jogoteca.py
+++ jogoteca.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    lista = ['Tetris','League of legends', 'The witcher']
+    return render_template('lista.html',titulo='Games', jogos = lista)
 
 app.run()
