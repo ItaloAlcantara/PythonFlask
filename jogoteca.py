4f8cfe56a104baf6f8fe81daa2c67f177b873e9b
Italo <italo.ads31@gmail.com>
2020-09-11 08:09:53 -0300

    dinamisando um objeto no html

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 
 @app.route('/inicio')
 def ola():
-    lista = ['Tetris','League of legends', 'The witcher']
-    return render_template('lista.html',titulo='Games', jogos = lista)
+    jogo1 = Jogo('Super Mario Bros', 'Ação', 'Nitendo')
+    jogo2 = Jogo('The Wicher', 'Aventura', 'Pc')
+    jogo3 = Jogo('Super Mario', 'Ação', 'Wii')
+
+    games = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Games', jogos=games)
 
 app.run()
